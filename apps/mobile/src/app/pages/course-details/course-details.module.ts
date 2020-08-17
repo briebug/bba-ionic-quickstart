94f6a23f9f8c7e8e024800b75a124f223522705f
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-08-16 21:29:24 -0700

    update: flattening pages folder structure

--- apps/mobile/src/app/pages/course-details/course-details.module.ts
+++ apps/mobile/src/app/pages/course-details/course-details.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { ComponentsModule } from '../../../components/components.module';
+import { ComponentsModule } from '../../components/components.module';
 import { CourseDetailsPageRoutingModule } from './course-details-routing.module';
 import { CourseDetailsPage } from './course-details.page';
 
--- apps/mobile/src/app/pages/lesson-details/lesson-details.module.ts
+++ apps/mobile/src/app/pages/lesson-details/lesson-details.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { ComponentsModule } from '../../../components/components.module';
+import { ComponentsModule } from '../../components/components.module';
 import { LessonDetailsPageRoutingModule } from './lesson-details-routing.module';
 import { LessonDetailsPage } from './lesson-details.page';
 
