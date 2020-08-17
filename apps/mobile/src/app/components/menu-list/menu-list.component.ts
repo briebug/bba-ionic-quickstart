97022bf9185abc2a2c678c6ae89c972daeff27f9
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-08-16 22:25:00 -0700

    update: using routerlinkactive

--- apps/mobile/src/app/components/menu-list/menu-list.component.ts
+++ apps/mobile/src/app/components/menu-list/menu-list.component.ts
@@ -1,35 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'bba-menu-list',
   templateUrl: './menu-list.component.html',
   styleUrls: ['./menu-list.component.scss'],
 })
-export class MenuListComponent implements OnInit {
+export class MenuListComponent {
   @Input() pages;
   @Input() dark;
   @Output() darkChange = new EventEmitter();
-
-  selectedIndex = 0;
-
-  ngOnInit() {
-    this.getSelectedPageIndex();
-  }
-
-  getSelectedPageIndex() {
-    const path = window.location.pathname.split('/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.pages.findIndex(
-        (page) => page.title.toLowerCase() === path.toLowerCase()
-      );
-    }
-  }
-
-  setSelectedPage(index) {
-    this.selectedIndex = index;
-  }
-
-  isSelectedPage(index) {
-    return this.selectedIndex === index;
-  }
 }
