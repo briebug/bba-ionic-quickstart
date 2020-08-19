b199ae5373290181ce51ff50628760d8b94659ea
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-08-19 14:55:20 -0700

    update: ionic theming

--- apps/mobile/src/app/components/menu-list/menu-list.component.ts
+++ apps/mobile/src/app/components/menu-list/menu-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'bba-menu-list',
@@ -7,4 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class MenuListComponent {
   @Input() pages;
+  @Input() dark;
+  @Output() darkChange = new EventEmitter();
 }
