1d9dbb0a95575d80a55f93ecc457133dab5b30d1
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-08-23 16:11:27 -0700

    update: cleaning up routing

--- apps/mobile/src/app/app.component.ts
+++ apps/mobile/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'bba-root',
@@ -32,19 +29,4 @@ export class AppComponent {
       icon: 'people',
     },
   ];
-
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
 }
