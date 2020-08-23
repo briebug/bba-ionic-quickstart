256d17a2d3265abe0649a410c446b0d3d3fcdaab
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-08-23 16:29:21 -0700

    update: cleaning up app files

--- apps/mobile/src/app/app.module.ts
+++ apps/mobile/src/app/app.module.ts
@@ -5,8 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { CoreDataModule } from '@bba/core-data';
 import { CoreStateModule } from '@bba/core-state';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,8 +23,6 @@ import { ComponentsModule } from './components/components.module';
     IonicModule.forRoot(),
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent],
